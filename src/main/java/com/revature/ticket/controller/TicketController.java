86068e6cb69a479f61be20562b34440859537aad
faithburg <faith974@revature.net>
2022-09-19 17:42:48 -0400

    added Cross Origin notations to connect to Angular

--- src/main/java/com/revature/ticket/controller/TicketController.java
+++ src/main/java/com/revature/ticket/controller/TicketController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ public class TicketController {
 	private TicketService ticketservice;
 
 	@GetMapping (path = "/admin/tickets")
+	@CrossOrigin(origins = "http://localhost:4200")
 	public List<Ticket> displayAllTickets() {
 		List<Ticket> tickets = new ArrayList<Ticket> ();
 		//logic to fetch ALL tickets from database
@@ -31,21 +33,25 @@ public class TicketController {
 	}
 	
 	@PostMapping (path = "/createticket")
+	@CrossOrigin(origins = "http://localhost:4200")
 	public Ticket saveTicket(@RequestBody Ticket ticket) {
 		return ticketservice.saveTicketToSQL(ticket);
 	}
 	
 	@GetMapping (path = "/admin/tickets/{id}")
+	@CrossOrigin(origins = "http://localhost:4200")
 	public Ticket displayTicketById(@PathVariable int id) {
 		return ticketservice.fetchTicketById(id).get();
 	}
 	
 	@GetMapping (path = "/admin/priority/{priority}")
+	@CrossOrigin(origins = "http://localhost:4200")
 	public List<Ticket> displayTicketById(@PathVariable String priority) {
 		return ticketservice.fetchTicketByPriority(priority);
 	}
 	
 	@DeleteMapping (path = "/admin/deleteticket/{id}")
+	@CrossOrigin(origins = "http://localhost:4200")
 	public String deleteTicketById(@PathVariable int id) {
 		return ticketservice.deleteTicketById(id);
 	}
